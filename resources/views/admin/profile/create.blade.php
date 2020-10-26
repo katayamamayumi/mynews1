f3e504e678e4d6361ffc4331719d0bdff1fdad52
katayama mayumi <mneko121@gmail.com>
2020-10-26 16:36:31 +0900

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.profile')
+
+@section('title', 'MY プロフィール')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-wd-8 mx-auto">
+      <h2>MY プロフィール</h2>
+    </div>
+  </div>
+</div>
+@endsection
